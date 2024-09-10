59a0047fa426cc6057e1996249e766913b201701
Harry <phuothuynh2002@gmail.com>
2024-09-11 01:07:28 +0700

    Remove unuse code

--- src/configs/TanStackProvider.tsx
+++ src/configs/TanStackProvider.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { useState } from 'react';
 
 const TanStackProvider = ({ children }: { children: React.ReactNode }) => {
   const [queryClient] = useState(
