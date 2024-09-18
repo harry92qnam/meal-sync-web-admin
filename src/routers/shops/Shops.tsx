908911b1b6bd4381ed3860aab943b8ca074e9157
Huỳnh Văn Phướt <62385893+harrycricket@users.noreply.github.com>
2024-09-19 03:32:36 +0700

    Create sidebar and header for layout (#16)

--- src/routers/shops/Shops.tsx
+++ src/routers/shops/Shops.tsx
@@ -1,15 +1,13 @@
 import Header from '../../components/common/Header';
 import MainLayout from '../../components/layout/MainLayout';
 
-export default function Dashboard() {
+export default function Shops() {
   return (
-    <MainLayout activeContentIndex={0}>
+    <MainLayout activeContentIndex={2}>
       <div className="md:col-span-1 py-4">
-        <Header title="Thống kê tổng quan" />
+        <Header title="Quản lý cửa hàng" />
       </div>
-      <div>
-        <p>Thống kê tổng quan</p>
-      </div>
+      <div>Quản lý cửa hàng</div>
     </MainLayout>
   );
 }
