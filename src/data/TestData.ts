9c26c0ea94e78d040fcca2b07324e602feb9e568
Harry <phuothuynh2002@gmail.com>
2024-09-29 16:56:15 +0700

    Update data sample for orders

--- src/data/TestData.ts
+++ src/data/TestData.ts
@@ -1156,4 +1156,7 @@ export const sampleOrders: FetchResponse<OrderModel> = {
     hasPrevious: false,
     hasNext: true,
   },
+  isSuccess: true, // {{ edit_1 }}
+  isFailure: false, // {{ edit_2 }}
+  error: { code: '', message: '' }, // {{ edit_3 }}
 };
