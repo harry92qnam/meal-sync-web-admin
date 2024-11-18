49b10edacea0463431aee46210b8621f2e668c30
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2024-11-18 10:37:37 +0700

    dashboard start

--- src/types/responses/DashboardResponse.ts
+++ src/types/responses/DashboardResponse.ts
@@ -2,35 +2,36 @@ import APICommonResponse from './APICommonResponse';
 
 export interface DashboardOverviewAPIReponse extends APICommonResponse {
   value: {
-    totalTrading: number;
-    totalTradingRate: number;
-    totalRevenue: number;
-    totalRevenueRate: number;
+    totalTradingAmount: number;
+    totalTradingAmountRate: number;
+    totalChargeFee: number;
+    totalChargeFeeRate: number;
     totalOrder: number;
     totalOrderRate: number;
     totalUser: number;
     totalUserRate: number;
-    dayCompareRate: number;
+    numDayCompare: number;
   };
 }
 
 export interface DashboardOrderDayInfo {
-  totalOfOrder: number;
-  totalTradingAmount: number;
-  revenue: number;
-  pending: number;
-  confirmed: number;
-  deliverying: number;
-  rejected: number;
-  cancelled: number;
-  successful: number;
-  failed: number;
+  totalOfOrder: number; // Tổng số đơn hàng
+  pending: number; // Số đơn hàng đang chờ xử lý
+  rejected: number; // Số đơn hàng bị từ chối
+  processingOrder: number; // Số đơn hàng đang xử lý
+  delivered: number; // Số đơn hàng đã giao
+  failDelivered: number; // Số đơn hàng giao thất bại
+  canceled: number; // Số đơn hàng bị hủy
+  successful: number; // Số đơn hàng thành công
+  issueProcessing: number; // Số đơn hàng đang giải quyết vấn đề
+  resolved: number; // Số vấn đề đã được giải quyết
+  totalTradingAmount: number; // Tổng giá trị giao dịch
+  totalChargeFee: number; // Tổng phí giao dịch
+  label: string; // Nhãn hoặc ngày tháng liên quan
 }
 
 export interface DashboardOrderAPIReponse extends APICommonResponse {
-  value: {
-    week: DashboardOrderDayInfo[];
-  };
+  value: DashboardOrderDayInfo[];
 }
 
 export interface DashboardRevenueAPIReponse extends APICommonResponse {
