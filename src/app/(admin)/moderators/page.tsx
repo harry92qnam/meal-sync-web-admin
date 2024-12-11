5e25aaffc2f4a9cd6ea6e84260c6a48a2be41518
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2024-12-11 10:30:57 +0700

    moderator table list

---
+++ src/app/(admin)/moderators/page.tsx
@@ -0,0 +1,138 @@
+'use client';
+import usePeriodTimeFilterState from '@/hooks/states/usePeriodTimeFilterQuery';
+import PageableModel from '@/types/models/PageableModel';
+import { Avatar, Selection } from '@nextui-org/react';
+import { NextPage } from 'next';
+import { ReactNode, useCallback, useEffect, useState } from 'react';
+
+import TableCustom, { TableCustomFilter } from '@/components/common/TableCustom';
+import useFetchWithRQWithFetchFunc from '@/hooks/fetching/useFetchWithRQWithFetchFunc';
+import apiClient from '@/services/api-services/api-client';
+import {
+  ModeratorModel,
+  ModeratorQuery,
+  moderatorQueryEmpty as emptyModeratorQuery,
+} from '@/types/models/ModeratorModel';
+import FetchResponse from '@/types/responses/FetchResponse';
+const columns = [
+  { key: 'id', name: '#' },
+  { key: 'avatarUrl', name: '' }, // Assuming you want to keep this column, you can add a custom name if needed
+  { key: 'fullName', name: 'Họ và tên' },
+  { key: 'email', name: 'Email' },
+  { key: 'phoneNumber', name: 'Số điện thoại' },
+  { key: 'status', name: 'Trạng thái' },
+  { key: 'dormitories', name: 'Khu quản lí' }, // You can modify this depending on how you want to display dormitories
+];
+const PromotionPage: NextPage = () => {
+  const { range, selected, setSelected, isSpecificTimeFilter } = usePeriodTimeFilterState();
+  const [statuses, setStatuses] = useState<Selection>(new Set(['0']));
+  const [query, setQuery] = useState<ModeratorQuery>(emptyModeratorQuery);
+  const fetcher = useFetchWithRQWithFetchFunc(
+    ['admin/moderator'],
+    async (): Promise<FetchResponse<ModeratorModel>> =>
+      apiClient
+        .get('admin/moderator', {
+          params: {
+            ...query,
+          },
+        })
+        .then((response) => response.data),
+    [query],
+  );
+
+  useEffect(() => {
+    setQuery((prevQuery) => ({
+      ...prevQuery,
+      dateFrom: range.dateFrom,
+      dateTo: range.dateTo,
+    }));
+  }, [range]);
+
+  const statusFilterOptions = [{ key: 0, desc: 'Tất cả' }]
+    .concat
+    // moderatorStatuses.map((item) => ({ key: item.key, desc: item.label })),
+    ();
+
+  const statusFilter = {
+    label: 'Trạng thái',
+    mappingField: 'status',
+    selectionMode: 1,
+    options: statusFilterOptions,
+    selectedValues: statuses,
+    handleFunc: (values: Selection) => {
+      const value = Array.from(values).map((val) => parseInt(val.toString()))[0];
+      setStatuses(values);
+      setQuery({ ...query, status: value, ...range });
+      console.log('Filter selected status: ', value);
+    },
+  } as TableCustomFilter;
+
+  const dormitoryList = [
+    { id: 1, name: 'Ký túc xá khu A' },
+    { id: 2, name: 'Ký túc xá khu B' },
+  ];
+  const dormitoryFilterOptions = [{ key: 0, desc: 'Tất cả' }].concat(
+    dormitoryList.map((item) => ({ key: item.id, desc: item.name })),
+  );
+
+  const renderCell = useCallback((moderator: ModeratorModel, columnKey: React.Key): ReactNode => {
+    const cellValue = moderator[columnKey as keyof ModeratorModel];
+
+    switch (columnKey) {
+      case 'id':
+        return (
+          <div className="flex flex-col">
+            <p className="text-bold text-small">MOD-{moderator.id}</p>
+          </div>
+        );
+      case 'avatarUrl':
+        return (
+          <div className="flex flex-col">
+            <Avatar src={moderator.avatarUrl} />
+          </div>
+        );
+      case 'dormitories':
+        return (
+          <div className="flex flex-col">
+            <p className="text-bold text-small">
+              {moderator.dormitories
+                .map((item) => (item.id == 1 ? 'KTX khu A' : 'KTX khu B'))
+                .join(', ')}
+            </p>
+          </div>
+        );
+      default:
+        return (
+          <div className="flex flex-col">
+            <p className="text-bold text-small">{cellValue.toString()}</p>
+          </div>
+        );
+    }
+  }, []);
+
+  return (
+    <div style={{ position: 'relative' }}>
+      <TableCustom
+        indexPage={2}
+        title="Quản lý điều phối viên"
+        placeHolderSearch="Tìm kiếm..."
+        description="Danh sách điều phối viên của nền tảng"
+        columns={columns}
+        total={fetcher.data?.value.totalCount ?? 0}
+        arrayData={fetcher.data?.value?.items ?? []}
+        searchHandler={(value: string) => {
+          setQuery({ ...query, searchValue: value.trim().toLowerCase() });
+        }}
+        pagination={fetcher.data?.value as PageableModel}
+        goToPage={(index: number) => setQuery({ ...query, pageIndex: index })}
+        setPageSize={(size: number) => setQuery({ ...query, pageSize: size })}
+        selectionMode="single"
+        filters={[statusFilter]}
+        renderCell={renderCell}
+        handleRowClick={() => {}}
+      />
+    </div>
+  );
+};
+
+export default PromotionPage;
---
+++ src/types/models/ModeratorModel.ts
@@ -0,0 +1,41 @@
+interface Dormitory {
+  id: number;
+  name: string;
+}
+
+export interface ModeratorModel {
+  id: number;
+  fullName: string;
+  email: string;
+  phoneNumber: string;
+  avatarUrl: string;
+  status: number;
+  dormitories: Dormitory[];
+}
+
+// Empty example data:
+export const emptyModerator: ModeratorModel = {
+  id: 0,
+  fullName: '',
+  phoneNumber: '',
+  email: '',
+  avatarUrl: '',
+  status: 0,
+  dormitories: [],
+};
+export interface ModeratorQuery {
+  searchValue: string;
+  status: number;
+  dormitoryId: number[];
+  pageIndex: number;
+  pageSize: number;
+}
+
+// Example of using the ModeratorQuery interface with the provided values:
+export const moderatorQueryEmpty: ModeratorQuery = {
+  searchValue: '',
+  status: 0,
+  dormitoryId: [],
+  pageIndex: 1,
+  pageSize: 20,
+};
